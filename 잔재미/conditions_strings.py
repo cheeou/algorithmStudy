c6e857e92d7418cf42e2a7d7e7a8c42b6500dbf2
Jang JiWoo <77304817+cheeou@users.noreply.github.com>
2024-10-24 08:48:07 +0900

    update dir 잔재미

---
+++ 잔재미/conditions_strings.py
@@ -0,0 +1,13 @@
+digit1 = int(input("Enter first number"))
+digit2 = int(input("Enter second number"))
+
+# if digit1 > digit2:
+#     print(digit1)
+# else:
+#     print(digit2)
+
+
+if digit1 % 2 == 0:
+    print("even")
+else:
+    print("odd")
