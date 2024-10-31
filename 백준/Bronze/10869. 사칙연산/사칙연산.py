657ca81e2ee447aaceeda47a85f2af69f15656e6
Jang JiWoo <77304817+cheeou@users.noreply.github.com>
2024-10-31 20:04:21 +0900

    [Bronze V] Title: 사칙연산, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,10 +1,7 @@
-a, b = input().split()
+a, b = map(int, input().split())
 
-a = int(a)
-b = int(b)
-
-print(a+b)
-print(a-b)
-print(a*b)
-print(a//b)
-print(a%b)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
